b27c79792a84cd8447566bf1359257eedb19e038
Maulana Tegar I <tegarmaulana973@gmail.com>
2024-03-19 22:54:57 +0700

    Mongoose mongo db compass validasi

--- server.js
+++ server.js
@@ -1,6 +1,54 @@
+require('dotenv/config') //panggil env
+const mongoose = require('mongoose')
 const app = require("./app");
 
-const PORT = 8000;
+const PORT = process.env.PORT; //panggil env yang tidak igin terlihat
+
+const DB = process.env.DATABASE
+
+mongoose
+.connect(DB, {
+  useNewUrlParser: true,
+  }).then(con => {
+    console.log('conncetion to database succes')
+    //console.log(con.connections)
+  })
+
+  const customerSchema = new mongoose.Schema({
+    name :  {
+      type: String,
+      required: [true, 'Name cannot be empty']
+    },
+    email: {
+      type: String,
+      unique: true, 
+    },
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+    city: String,
+    country: {
+      type: String,
+      required: true,
+      default: 'Indonesia',
+    },
+
+  })
+
+  const Customer = mongoose.model('Customer', customerSchema)
+
+  const customerTest = new Customer({
+    name: 'yundilaii',
+    email: 'yundiilkauuu@gmail.com',
+    phoneNumber: '8977777777',
+  })
+
+  customerTest.save().then(doc => {
+    console.log(doc)
+  }).catch(err => {
+    console.log('ERROR : ' + err)
+  })
 
 app.listen(PORT, () => {
   console.log(`APP running on port : ${PORT}`);
